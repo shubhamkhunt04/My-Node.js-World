87a1ded7bbe35ee0c241706d8f59aa31e2e250d2
shubham khunt <shubhamkhunt08@gmail.com>
2020-03-13 20:02:44 +0530

    global object in node

---
+++ nodejs practice/4_global_object.js
@@ -0,0 +1,14 @@
+// __dirname
+// __filename
+// require
+// console
+// buffer
+// module
+// exports
+
+
+console.log(__dirname);
+console.log(__filename);
+
+var temp = require('./functions') // all values are printed which is indside the functions.js module.
+console.log(temp.myname)   //  access functions.js module variable.
