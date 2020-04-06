b38a986621e6662aec5403caaf8d64abddd48539
shubham khunt <shubhamkhunt08@gmail.com>
2020-04-06 21:03:10 +0530

    node mongoose 2

---
+++ nodejs practice/21_node-mongoose/index.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log('Connected correctly to server');
+
+    Dishes.create({
+        name: 'pizza',
+        description: 'test pizz'
+    })
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.findByIdAndUpdate(dish._id,{
+                $set:{description:"updated test"},
+                new:true
+            }).exec();
+        })
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating:5,
+                comment : "super dishe shubham",
+                author:'shubham khunt'
+            });
+
+            return dish.save();
+
+        })
+        .then((dish)=>{
+            console.log(dish);
+
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
