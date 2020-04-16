38f45c42f4b8b3ee867f88b668f79990d1b9d84f
shubham khunt <shubhamkhunt08@gmail.com>
2020-04-16 08:27:53 +0530

    passport authentication

---
+++ nodejs practice/27_passport_auth/models/user.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose'); // for storing username and password(hash formate)
+
+var User = new Schema({
+    admin:{
+        type:Boolean,
+        default:false
+    }
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User',User);
