32974231ec6a3a11b2c08febd60bfb75a0c2807a
shubham khunt <shubhamkhunt08@gmail.com>
2020-04-23 22:07:38 +0530

    mongoose population in nodejs

---
+++ nodejs practice/29_Mongoose_Population/routes/promoRouter.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const authenticate = require("../authenticate");
+
+const Promo = require("../models/promotions"); 
+
+const promotions = express.Router();
+
+promotions.use(bodyParser.json());
+
+promotions.route('/')
+.get((req,res,next)=>{
+    Promo.find({})
+    .then((promotion)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(promotion);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
+})
+.post(authenticate.verifyUser, (req,res,next)=>{
+    Promo.create(req.body)
+    .then((promotion)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(promotion);
+    },
+        (err)=>next(err))
+    .catch((err)=>next(err));
+})
+.put(authenticate.verifyUser, (req,res,next)=>{
+    res.statusCode = 403;
+    res.end('PUT operation dose not support on /dishes');
+})
+.delete(authenticate.verifyUser, (req,res,next)=>{
+    Promo.remove({})
+    .then((resp)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(resp);
+    },
+    (err)=>next(err))
+.catch((err)=>next(err));
+});
+
+
+
+
+promotions.route('/:promotionsId')
+.get((req,res,next)=>{
+    Promo.findById(req.params.promotionsId)
+    .then((promotion)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(promotion);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
+})
+.post(authenticate.verifyUser, (req,res,next)=>{
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'+req.params.promotionsId);
+})
+.put(authenticate.verifyUser, (req,res,next)=>{
+    Promo.findByIdAndUpdate(req.params.promotionsId,{
+        $set:req.body
+    },{new :true})
+    .then((promotion)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(promotion);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
+})
+.delete(authenticate.verifyUser, (req,res,next)=>{
+    Promo.findByIdAndRemove(req.params.promotionsId)
+    .then((resp)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(resp);
+    },
+    (err)=>next(err))
+.catch((err)=>next(err));
+});
+
+
+
+module.exports = promotions;
