a6ebd1d8b4d90962fcb24ab5eb658ffad3becac2
shubham khunt <shubhamkhunt08@gmail.com>
2020-05-07 10:58:19 +0530

    quiz web

---
+++ nodejs practice/quiz/models/dishes.js
@@ -0,0 +1,46 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// require('mongoose-currency').loadType(mongoose);
+// const Currency = mongoose.Types.Currency;
+
+// const optionSchema = new Schema({
+//    /* qustion: {
+//         type: String,
+//         min: 1,
+//         max: 2,
+//         required: true
+//     },
+//     author: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref:'User'   
+//     },  */
+//     option:{
+//         type: String,
+//         required: true,
+//         max:4
+//     },
+//     answer:{
+//         type:String,
+//         required:true
+//     }
+// }, {
+//     timestamps: true
+// });
+
+
+// const qustionSchema = new Schema({
+//     qustion : {
+//         type: String,
+//         required: true,
+//         min:1,
+//         unique: true
+//     },
+//     options: [optionSchema]
+// }, {
+//     timestamps: true
+// });
+
+// var Qustion = mongoose.model('Qustion', qustionSchema);
+
+// module.exports = Qustion;
